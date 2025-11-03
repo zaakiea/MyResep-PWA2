5910a9d5d837a5bc7c13f178883e1a45a9802b64
zaakiea <dzaki1atmaja@gmail.com>
2025-11-03 08:57:12 +0700

    Fix: Ubah sumber pwa-assets ke LOGORN.png

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,12 +1,12 @@
 import {
-    defineConfig,
-    minimal2023Preset as preset,
-} from '@vite-pwa/assets-generator/config'
+  defineConfig,
+  minimal2023Preset as preset,
+} from "@vite-pwa/assets-generator/config";
 
 export default defineConfig({
-    headLinkOptions: {
-        preset: '2023',
-    },
-    preset,
-    images: ['public/favicon.ico'],
-})
+  headLinkOptions: {
+    preset: "2023",
+  },
+  preset,
+  images: ["public/LOGORN.png"], // <-- UBAH BARIS INI
+});
